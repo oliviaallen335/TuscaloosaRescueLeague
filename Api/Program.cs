93e443ebb81c0eb7cc66063eef867dc5e53c90b6
Olivia Allen <olivia.allen335@gmail.com>
2026-04-21 03:22:19 -0500

    ugh

--- Api/Program.cs
+++ Api/Program.cs
@@ -74,7 +74,8 @@ builder.Services.AddCors(options =>
                 "http://127.0.0.1:5500",
                 "http://127.0.0.1:3000",
                 "http://127.0.0.1:5173",
-                "http://127.0.0.1:8080")
+                "http://127.0.0.1:8080",
+                "https://oliviaallen335.github.io")
             .AllowAnyMethod()
             .AllowAnyHeader();
     });
